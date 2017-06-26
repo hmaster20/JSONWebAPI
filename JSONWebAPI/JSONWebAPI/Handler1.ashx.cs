9a7da66d45ac0de597c31571f7afb14344b797e7
hmaster <hmaster20@gmail.com>
2017-06-26 23:25:53 +0300

    Part 1

---
+++ JSONWebAPI/JSONWebAPI/Handler1.ashx.cs
@@ -0,0 +1,40 @@
+﻿using JsonServices;
+using JsonServices.Web;
+using JSONWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JSONWebAPI
+{
+    /// <summary>
+    /// Сводное описание для Handler1
+    /// </summary>
+    //public class Handler1 : IHttpHandler
+    public class Handler1 : JsonHandler
+    {
+        public Handler1()
+        {
+            this.service.Name = "JSONWebAPI";
+            this.service.Description = "JSON API for android appliation";
+            InterfaceConfiguration IConfig = new InterfaceConfiguration("RestAPI", typeof(IServiceAPI), typeof(ServiceAPI));
+            this.service.Interfaces.Add(IConfig);
+        }
+
+
+        //public void ProcessRequest(HttpContext context)
+        //{
+        //    context.Response.ContentType = "text/plain";
+        //    context.Response.Write("Привет всем!");
+        //}
+
+        //public bool IsReusable
+        //{
+        //    get
+        //    {
+        //        return false;
+        //    }
+        //}
+    }
+}
---
+++ JSONWebAPI/SQLQuery1.sql
@@ -0,0 +1,30 @@
+﻿-------------Table 1: UserDetails (To store user information)---------------
+ 
+CREATE TABLE UserDetails (
+      id INT IDENTITY,
+      firstName VARCHAR(50),
+      lastName VARCHAR(50),
+      userName VARCHAR(50),
+      password VARCHAR(50),
+);
+ 
+-------------Table 2: Dept (To store Department information)---------------
+ 
+CREATE TABLE Dept( 
+      no INT,
+      name VARCHAR(50),
+       PRIMARY KEY (no)
+);
+ 
+----------------------Insert data to Dept Table------------------
+ 
+INSERT INTO Dept VALUES (1,'Accounting');
+INSERT INTO Dept VALUES (2,'Marketing');
+INSERT INTO Dept VALUES (3,'Information Technology');
+INSERT INTO Dept VALUES (4,'Networking');
+INSERT INTO Dept VALUES (5,'Management');
+INSERT INTO Dept VALUES (6,'Medical');
+INSERT INTO Dept VALUES (7,'Electronics');
+INSERT INTO Dept VALUES (8,'Finance');
+INSERT INTO Dept VALUES (9,'Engineering');
+INSERT INTO Dept VALUES (10,'Defense');
